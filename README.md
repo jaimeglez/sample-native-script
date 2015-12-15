e4cb1c510848bc0c882f1e6b2863ea0835501333
Jaime Gonzalez <sacemax21@gmail.com>
2015-12-15 11:29:59 -0600

    get the lat and lon

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # sample-native-script
-This is a simple sample about how to use native-script using modules from npm, using views, API calls, etc. 
+This is a simple sample about how to use native-script using modules from npm, using views, API calls, etc.
 
     npm install
     tns livesync android --watch
+
+    echo 'geo fix -99.133333 19.43333 2202' | nc localhost 5554
--- app/views/unit/unit.js
+++ app/views/unit/unit.js
@@ -10,26 +10,38 @@ var locationManager = new locationModule.LocationManager();
 var locationOptions = {
     desiredAccuracy: 3,
     updateDistance: 0,
-    minimumUpdateTime: 5000,
-    maximumAge: 20000
+    minimumUpdateTime: 0,
+    timeout: 10000,
+    maximumAge: 0
+};
+
+function setPosition(latitude, longitude){
+  console.log(latitude + " " + longitude)
+};
+
+function showEnableLocation(){
+  appModule.android.currentContext.startActivityForResult(new android.content.Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS), 0); // show the config to enabled gps
+};
+
+function startNotifyAvailability(){
+  if (!isEnabled){
+    showEnableLocation();
+  }else{
+    locationManager.startLocationMonitoring(function(location){
+      setPosition(location.latitude, location.longitude)
+    }, function (error) {
+      alert("Location error received: " + error);
+    }, locationOptions);
+  }
 };
 
 exports.pageLoaded = function(args) {
   var page = args.object;
   page.bindingContext = page._navigationContext;
+  startNotifyAvailability();
 };
 
-exports.testing = function(args){
-  var page = args.object;
-  alert(isEnabled.toString());
-  if (!isEnabled){
-    appModule.android.currentContext.startActivityForResult(new android.content.Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS), 0); // show the config to enabled gps
-  }else{
-    alert('Im in');
-    locationManager.startLocationMonitoring(function(location){
-      console.log("Location received: " + location);
-    }, function (error) {
-      console.log("Location error received: " + error);
-    }, locationOptions);
-  }
+exports.stopNotifyAvailability = function(){
+  locationManager.stopLocationMonitoring();
 };
+
