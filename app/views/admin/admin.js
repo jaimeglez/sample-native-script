c6b4c85cfe7f7543bdefbe5a3778db0bc92bf61a
Jaime Gonzalez <sacemax21@gmail.com>
2015-12-16 19:45:22 -0600

    added 'typeahead', just remainind the add and delete methods

--- app/views/admin/admin.js
+++ app/views/admin/admin.js
@@ -10,17 +10,19 @@ var db = dbModule.openDb(dbName);
 var observableModule = require("data/observable");
 var observableArrayModule = require("data/observable-array");
 var pageData = new observableModule.Observable({
+  allRoutes: new observableArrayModule.ObservableArray([]),
   routes: new observableArrayModule.ObservableArray([]),
   selectedRoutes: new observableArrayModule.ObservableArray([]),
-  search: ''
+  search: '',
+  noRoutes: 'hidden'
 });
 
 var textFieldModule = require("ui/text-field");
 
 function fillLists(results){
   setTimeout(function(){
-    pageData.set('routes', _.where(results, {available: 0}));
-    pageData.set('selectedRoutes', _.where(results, {available: 0}));
+    pageData.set('allRoutes', _.where(results, {available: 0}));
+    pageData.set('selectedRoutes', _.where(results, {available: 1}));
   },100);
 };
 
@@ -36,15 +38,32 @@ function getRoutes(){
 exports.pageLoaded = function(args) {
   var page = args.object;
   search = page.getViewById('search');
+  routesList = page.getViewById('routes-list');
   page.bindingContext = pageData;
   getRoutes();
 };
 
 exports.testing = function() {
-  console.log(inspect(pageData.get('search')));
+  // console.log(inspect(pageData.get('search')));
 };
 
 pageData.on(observableModule.Observable.propertyChangeEvent, function(propertyChangeData){
-  // filter(propertyChangeData.value);
-  console.log('changeEvent');
+  query = propertyChangeData.value;
+  property = propertyChangeData.propertyName;
+  console.log(property)
+  if (property == "search"){
+    if (query != ""){
+      filteredItems = _.filter(pageData.get('allRoutes'), function(item){ return item.name.indexOf(query) > -1; });
+      if (filteredItems.length > 0){
+        pageData.set('routes', filteredItems);
+        console.log('ago');
+        pageData.set('noRoutes', 'show');
+      }else{
+        console.log('nada');
+        pageData.set('noRoutes', 'hidden');
+      }
+    }else{
+      pageData.set('noRoutes', 'hidden');
+    }
+  }
 });
